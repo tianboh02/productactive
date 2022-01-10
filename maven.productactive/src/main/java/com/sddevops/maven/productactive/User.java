8ce0b055622d5b9a8d33f0da6351ca86a2fa8b47
mousouexpress <ivanhowenjie@gmail.com>
2022-01-10 16:54:11 +0800

    Added User Class with attributes

---
+++ maven.productactive/src/main/java/com/sddevops/maven/productactive/User.java
@@ -0,0 +1,9 @@
+package com.sddevops.maven.productactive;
+
+public class User {
+	private int id;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+}
