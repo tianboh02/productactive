8692df8336169b67c64ba6bac13489f82d91770c
tianboh02 <khawtianboh.ktb@gmail.com>
2022-01-18 09:32:52 +0800

    Revert "Revert "Created ActivityLogger Class""
    This reverts commit 7a97357c3e19c25218972570bb9f91ecca4c6056.

---
+++ maven.productactive/src/main/java/com/sddevops/maven/productactive/ActivityLogger.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven.productactive;
+
+import java.time.LocalDateTime;
+
+public class ActivityLogger {
+
+	private int id;
+	private int userId;
+	private String activityName;
+	private String activityDesription;
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+}
