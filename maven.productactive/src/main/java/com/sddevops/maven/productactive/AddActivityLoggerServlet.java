dbfb627db7da531310f6d4ad8023e3bc37761a91
tianboh02 <khawtianboh.ktb@gmail.com>
2022-02-12 16:50:09 +0800

    updated servlet file

--- maven.productactive/src/main/java/com/sddevops/maven/productactive/AddActivityLoggerServlet.java
+++ maven.productactive/src/main/java/com/sddevops/maven/productactive/AddActivityLoggerServlet.java
@@ -82,7 +82,6 @@ public class AddActivityLoggerServlet extends HttpServlet {
 			ps.setString(5, e);
 			//Step 6: perform the query on the database using the prepared statement
 			int i = ps.executeUpdate();
-			//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response,
 			if (i > 0){
 				response.sendRedirect("http://localhost:8090/maven.productactive/ActivityLoggerServlet/dashboard");
 			}
