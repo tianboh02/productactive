c1e5684c0415dd08e32f714e5abb81ca26269f77
tianboh02 <khawtianboh.ktb@gmail.com>
2022-02-14 20:28:58 +0800

    HomePageSeleniumTest.java created

---
+++ maven.productactive/src/test/java/com/sddevops/maven/productactive/HomePageSeleniumTest.java
@@ -0,0 +1,19 @@
+package com.sddevops.maven.productactive;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class HomePageSeleniumTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
