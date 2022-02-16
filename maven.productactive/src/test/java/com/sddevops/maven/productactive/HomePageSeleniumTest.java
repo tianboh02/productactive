3661ed6db674559b9c5a6e7b3fe6ca929553f557
tianboh02 <khawtianboh.ktb@gmail.com>
2022-02-16 11:56:55 +0800

    home page selenium comments

--- maven.productactive/src/test/java/com/sddevops/maven/productactive/HomePageSeleniumTest.java
+++ maven.productactive/src/test/java/com/sddevops/maven/productactive/HomePageSeleniumTest.java
@@ -2,11 +2,35 @@ package com.sddevops.maven.productactive;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class HomePageSeleniumTest {
   @Test
   public void f() {
+	  
+		// define the chrome driver
+		System. setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+		// define the drive instance
+		WebDriver driver = new ChromeDriver();
+				
+		//1. home page
+		//2. login
+		//3. home page
+		//4. activity logger dashboard
+		//5. home page
+		//6. notepad dashboard
+		//7. home page
+		//8. deadline dashboard
+		//9. home page
+		//10. account page
+		//11. home page
+		//12. log out
+		//13. home page
+		
   }
   @BeforeTest
   public void beforeTest() {
