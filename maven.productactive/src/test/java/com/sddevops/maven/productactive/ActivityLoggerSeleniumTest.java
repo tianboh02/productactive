7f3aeaff2722bc6c3f60af1840247b6108a6a141
tianboh02 <khawtianboh.ktb@gmail.com>
2022-02-16 21:01:37 +0800

    Activity Logger Selenium completed

--- maven.productactive/src/test/java/com/sddevops/maven/productactive/ActivityLoggerSeleniumTest.java
+++ maven.productactive/src/test/java/com/sddevops/maven/productactive/ActivityLoggerSeleniumTest.java
@@ -3,6 +3,8 @@ package com.sddevops.maven.productactive;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -15,80 +17,64 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class ActivityLoggerSeleniumTest {
-  @Test
-  public void f() {
+	
+	//routes
+	String home = "http://localhost:8080/maven.productactive/HomeServlet";
+	String login = "http://localhost:8080/maven.productactive/LoginServlet";
+	String dashboard = "http://localhost:8080/maven.productactive/ActivityLoggerServlet/dashboard";
+	String addLog = "http://localhost:8080/maven.productactive/AddActivityLoggerServlet";
+	String editLog;
+	
+	
+
+  @Test (priority = 0)
+  public void logDashboard() {
+		// define the chrome driver
+		System. setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+		// define the drive instance
+		WebDriver driver = new ChromeDriver();
+			   
+			  
+		driver.get(home);
+			  
+		driver.findElement(By.linkText ("Login")) .click();
+			  
+		//2. login page
+		driver.findElement(By.name("username")).sendKeys("Tianboh");
+		driver.findElement(By.name("password")).sendKeys("123456789");
+			  
+		driver.findElement(By.className("loginButton")).click();
+			  
+		driver.findElement(By.linkText ("Activity Logger")) .click();
+		
+		driver.quit();
+		  
+  }
+  
+  @Test (priority = 1)
+  public void logAdd() {
 	  // define the chrome driver
 	  System. setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
 	  // define the drive instance
 	  WebDriver driver = new ChromeDriver();
+	   
 	  
-	  //routes
-	  String home = "http://localhost:8080/maven.productactive/HomeServlet";
-	  String login = "http://localhost:8080/maven.productactive/LoginServlet";
-	  String dashboard = "http://localhost:8080/maven.productactive/ActivityLoggerServlet/dashboard";
-	  String addLog = "http://localhost:8080/maven.productactive/AddActivityLoggerServlet";
-	  String editLog;
-	  
-	  String currenturl;
-	  
-	  // 1. Home page
 	  driver.get(home);
-	  
-	  currenturl = driver.getCurrentUrl();
-	  if(currenturl == home) {
-		  System.out.println("1. Redirected to home page (pass)");
-	  }
-	  else {
-		  System.out.println("1. Page is not redirected correctly (fail)");
-	  }
 	  
 	  driver.findElement(By.linkText ("Login")) .click();
 	  
 	  //2. login page
-	  if(driver.getCurrentUrl() == login) {
-		  System.out.println("2. Redirected to login page (pass)");
-	  }
-	  else {
-		  System.out.println("2. Page is not redirected correctly (fail)");
-	  }
-	  
 	  driver.findElement(By.name("username")).sendKeys("Tianboh");
 	  driver.findElement(By.name("password")).sendKeys("123456789");
-	  System.out.println("User information filled in.");
 	  
 	  driver.findElement(By.className("loginButton")).click();
-	  System.out.println("Login Submit");
-	  
-	  //3. home page
-	  if(driver.getCurrentUrl() == home) {
-		  System.out.println("3. Redirected to home page (pass)");
-	  }
-	  else {
-		  System.out.println("3. Page is not redirected correctly (fail)");
-	  }
 	  
 	  driver.findElement(By.linkText ("Activity Logger")) .click();
 	  
-	  //4. activity logger dashboard
-	  if(driver.getCurrentUrl() == dashboard) {
-		  System.out.println("4. Redirected to activity logger dashboard page (pass)");
-	  }
-	  else {
-		  System.out.println("4. Page is not redirected correctly (fail)");
-	  }
-	  
 	  driver.findElement(By.linkText ("Log New Activity")) .click();
 	  
-	  //5. add new log
-	  if(driver.getCurrentUrl() == addLog) {
-		  System.out.println("5. Redirected to add activity logger page (pass)");
-	  }
-	  else {
-		  System.out.println("5. Page is not redirected correctly (fail) ");
-	  }
 	  driver.findElement(By.name("activity_name")).sendKeys("History");
 	  driver.findElement(By.name("activity_description")).sendKeys("practice paper");
-	  
 	  WebElement dateBoxStart = driver.findElement(By.xpath("//*[@id=\"activity_start\"]"));
 	  WebElement dateBoxEnd = driver.findElement(By.xpath("//*[@id=\"activity_end\"]"));
 	  dateBoxStart.sendKeys("01112022");
@@ -98,12 +84,81 @@ public class ActivityLoggerSeleniumTest {
 	  dateBoxEnd.sendKeys(Keys.TAB);
 	  dateBoxEnd.sendKeys("1050PM");
 
-	  System.out.println("Log information filled in.");
 	  driver.findElement(By.name("submit")).click();
-	  System.out.println("Log Submit");
-	  
+	  driver.quit();
 
   }
+  
+  @Test (priority = 2)
+  public void logEdit() {
+	// define the chrome driver
+		  System. setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+		  // define the drive instance
+		  WebDriver driver = new ChromeDriver();
+		   
+		  
+		  driver.get(home);
+		  
+		  driver.findElement(By.linkText ("Login")) .click();
+		  
+		  //2. login page
+		  driver.findElement(By.name("username")).sendKeys("Tianboh");
+		  driver.findElement(By.name("password")).sendKeys("123456789");
+		  
+		  driver.findElement(By.className("loginButton")).click();
+		  
+		  driver.findElement(By.linkText ("Activity Logger")) .click();
+		  
+
+		  driver.findElement(By.xpath("//td[contains(text(),'practice paper')]/following-sibling::td//a[contains(text(),'Edit')]")).click();
+		  
+		  WebElement name = driver.findElement(By.name("activityName"));
+		  WebElement description = driver.findElement(By.name("activityDescription"));
+		  WebElement dateBoxStart = driver.findElement(By.xpath("//input[@name='startDateTime']"));
+		  WebElement dateBoxEnd = driver.findElement(By.xpath("//input[@name='endDateTime']"));
+		  name.clear();
+		  description.clear();
+		  name.sendKeys("History");
+		  description.sendKeys("actual paper");
+		  dateBoxStart.sendKeys("02112022");
+		  dateBoxStart.sendKeys(Keys.TAB);
+		  dateBoxStart.sendKeys("0100PM");
+		  dateBoxEnd.sendKeys("02112022");
+		  dateBoxEnd.sendKeys(Keys.TAB);
+		  dateBoxEnd.sendKeys("0250PM");
+		  
+		  driver.findElement(By.xpath("//button[normalize-space()='Save']")).click();
+		  driver.quit();
+
+  }
+  
+  @Test (priority = 3)
+  public void logDelete() {
+	// define the chrome driver
+	  System. setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+	  // define the drive instance
+	  WebDriver driver = new ChromeDriver();
+	   
+	  
+	  driver.get(home);
+	  
+	  driver.findElement(By.linkText ("Login")) .click();
+	  
+	  //2. login page
+	  driver.findElement(By.name("username")).sendKeys("Tianboh");
+	  driver.findElement(By.name("password")).sendKeys("123456789");
+	  
+	  driver.findElement(By.className("loginButton")).click();
+	  
+	  driver.findElement(By.linkText ("Activity Logger")) .click();
+	  
+	  driver.findElement(By.xpath("//td[contains(text(),'actual paper')]/following-sibling::td//a[contains(text(),'Delete')]")).click();
+	  
+	  System.out.println("Log deleted");
+	  driver.quit();
+  }
+  
+ 
   @BeforeTest
   public void beforeTest() {
   }
