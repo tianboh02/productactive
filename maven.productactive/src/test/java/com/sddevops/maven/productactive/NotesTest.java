89da55ee16c48ab7c8ae8c64a7bb755f47432c46
J-Yang139 <jing139@yahoo.com>
2022-02-21 20:13:19 +0800

    Notetest

--- maven.productactive/src/test/java/com/sddevops/maven/productactive/NotesTest.java
+++ maven.productactive/src/test/java/com/sddevops/maven/productactive/NotesTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterTest;
 
 public class NotesTest {
   @Test
-  public void f() {
+  public void f() throws InterruptedException {
 	  
 	System. setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
 	
@@ -19,24 +19,30 @@ public class NotesTest {
 	driver.get ("http://localhost:8080/maven.productactive/HomeServlet");
 	
 	driver.findElement(By.linkText("Login")).click();
+	Thread.sleep(2000);
 
 	driver.findElement(By.name("username")).sendKeys("a");
 	driver.findElement(By.name("password")).sendKeys("a");
 	driver.findElement(By.className("loginButton")).submit();
+	Thread.sleep(2000);
 	
 	driver.findElement(By.linkText ("Note Pad")) .click();
+	Thread.sleep(2000);
 	
 	driver.findElement(By.linkText ("Add New Note")) .click();
 	driver.findElement(By.name("title")).sendKeys("Automation Test");
 	driver.findElement(By.name("content")).sendKeys("Automation Test Content");
 	driver.findElement(By.className("btn-success")).submit();
+	Thread.sleep(2000);
 	
 	driver.findElement(By.xpath("//a[@href='edit?id=6']")).click();
 	driver.findElement(By.name("title")).sendKeys(" (Edited)");
 	driver.findElement(By.name("content")).sendKeys(" (Edited)");
 	driver.findElement(By.className("btn-success")).submit();
+	Thread.sleep(2000);
 	
 	driver.findElement(By.xpath("//a[@href='delete?id=6']")).click();
+	Thread.sleep(2000);
 	
 	driver.quit();
   }
